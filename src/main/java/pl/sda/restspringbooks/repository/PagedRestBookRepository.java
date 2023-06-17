f00f523efc427bddcb88e035ff0981881812972d
cs <siwon.cezary@gmail.com>
2023-06-17 20:39:13 +0200

    end of day 5

--- src/main/java/pl/sda/restspringbooks/repository/PagedRestBookRepository.java
+++ src/main/java/pl/sda/restspringbooks/repository/PagedRestBookRepository.java
@@ -8,9 +8,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import pl.sda.restspringbooks.model.Book;
 
+import java.util.Collection;
+
 @RepositoryRestResource(path = "books", collectionResourceRel = "books")
 public interface PagedRestBookRepository extends PagingAndSortingRepository<Book, Long> {
-    // localhost:9000/books/search/authors?year=2000&sort=title
-    @RestResource(path = "edition")
+    // localhost:9000/books/search/edition?year=2000&sort=title
+    @RestResource(path = "edition", rel = "edition")
     Page<Book> findBooksByEditionYear(@Param("year") int year, Pageable p);
+
+    @RestResource(path = "authors", rel = "authors")
+    Page<Book> findBooksByAuthorsContaining(@Param("author") String author, Pageable p);
 }
