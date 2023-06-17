945d0e6af146c6276e4d62b24e392395c9dbe414
cs <siwon.cezary@gmail.com>
2023-06-17 14:28:09 +0200

    add validation.

--- src/main/java/pl/sda/restspringbooks/controller/RestBookController.java
+++ src/main/java/pl/sda/restspringbooks/controller/RestBookController.java
@@ -1,2 +1,100 @@
-package pl.sda.restspringbooks.controller;public class RestBookController {
+package pl.sda.restspringbooks.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+import pl.sda.restspringbooks.model.Book;
+
+import java.net.URI;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class RestBookController {
+    private List<Book> books = new ArrayList<>(
+            List.of(
+                    Book
+                            .builder()
+                            .id(1)
+                            .title("Java")
+                            .editionYear(2020)
+                            .build(),
+                    Book
+                            .builder()
+                            .id(2)
+                            .title("Spring")
+                            .editionYear(2021)
+                            .build()
+            )
+    );
+
+    @GetMapping("")
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Book> createBook(@Valid @RequestBody Book book){
+        final OptionalLong max = books.stream().mapToLong(b -> b.getId()).max();
+        book.setId(max.getAsLong() + 1);
+        books.add(book);
+        return ResponseEntity
+                .created(URI.create("/api/v1/books/"+book.getId()))
+                .build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(
+            @RequestParam long id,
+            @Valid @RequestBody Book book
+    ){
+        if (id != book.getId()){
+            return ResponseEntity.badRequest().build();
+        }
+        final Optional<Book> optionalBook = books
+                .stream()
+                .filter(b -> b.getId() == id)
+                .findFirst();
+        if (optionalBook.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        books.set(books.indexOf(optionalBook.get()), book);
+        return ResponseEntity.ok(book);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable long id) {
+        final Optional<Book> optionalBook = books
+                .stream()
+                .filter(b -> b.getId() == id)
+                .findFirst();
+        return ResponseEntity.of(optionalBook);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable long id){
+        final Optional<Book> optionalBook = books
+                .stream()
+                .filter(b -> b.getId() == id)
+                .findFirst();
+        if (optionalBook.isPresent()){
+            books.remove(optionalBook.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, Object> handleValidationErrors(MethodArgumentNotValidException exception){
+        Map<String, Object> errors = new HashMap<>();
+        exception
+                .getBindingResult()
+                .getFieldErrors()
+                .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return errors;
+    }
 }
--- src/main/java/pl/sda/restspringbooks/model/Book.java
+++ src/main/java/pl/sda/restspringbooks/model/Book.java
@@ -1,2 +1,18 @@
-package pl.sda.restspringbooks.model;public class Book {
+package pl.sda.restspringbooks.model;
+
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@Builder
+public class Book {
+    private long id;
+
+    @Length(max = 50, min = 2, message = "Tytuł musi mieć od 2 do 50 znaków!")
+    private String title;
+
+    @Range(min = 2000, message = "Rok wydania nie może być mniejszy od 2000!")
+    private int editionYear;
 }
